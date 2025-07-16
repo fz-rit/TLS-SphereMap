fcd41ff12ade1722c556dbc065ed034274f2e24c
Fei Zhang <fzhcis@rit.edu>
2025-07-16 14:56:28 -0400

    feat: Enable default geometry feature calculation in processing pipeline

--- run_3d_to_2d_pipeline.py
+++ run_3d_to_2d_pipeline.py
@@ -38,7 +38,7 @@ def main():
 
     # Define default processing steps
     default_scripts = [
-        # "processing.calc_geom_feature",
+        "processing.calc_geom_feature",
         "processing.spherical_projection", 
         "processing.spherical_back_projection",
         # "data_annotation.seg_map_tools.convert_color_to_mask", # Only if 2D segmentation map (color) is available.
